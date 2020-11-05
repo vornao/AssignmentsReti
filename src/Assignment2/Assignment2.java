fe8663f37513b8691efd483e48e9876b2c6dc445
vornao <migliorluca@gmail.com>
2020-11-05 16:08:00 +0100

    Added Assignment 1 to 5

--- src/Assignment2/Assignment2.java
+++ src/Assignment2/Assignment2.java
@@ -2,7 +2,24 @@ package Assignment2;
 
 import java.util.ArrayList;
 import java.util.concurrent.RejectedExecutionException;
-import java.util.concurrent.ThreadPoolExecutor;
+
+
+/**
+ * Simulare il flusso di clienti in un ufficio postale che ha 4 sportelli. Nell'ufficio esiste:
+ * un'ampia sala d'attesa in cui ogni persona può entrare liberamente. Quando entra,
+ * ogni persona prende il numero dalla numeratrice e aspetta il proprio turno in questa sala.
+ * una seconda sala, meno ampia, posta davanti agli sportelli, in cui possono essere presenti al massimo k persone
+ * Una persona si mette quindi prima in coda nella prima sala, poi passa nella seconda sala.
+ * Ogni persona impiega un tempo differente per la propria operazione allo sportello.
+ * Una volta terminata l'operazione, la persona esce dall'ufficio
+ *
+ * Scrivere un programma in cui:
+ * - l'ufficio viene modellato come una classe JAVA, in cui viene attivato un ThreadPool di dimensione uguale al numero degli sportelli
+ * - la coda delle persone presenti nella sala d'attesa è gestita esplicitamente dal programma
+ * - la seconda coda (davanti agli sportelli) è quella gestita implicitamente dal ThreadPool
+ * - ogni persona viene modellata come un task, un task che deve essere assegnato ad uno dei thread associati agli sportelli
+ * - si preveda di far entrare tutte le persone nell'ufficio postale, all'inizio del programma
+ */
 
 public class Assignment2 {
 
